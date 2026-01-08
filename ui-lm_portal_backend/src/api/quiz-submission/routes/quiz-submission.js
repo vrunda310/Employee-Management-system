18c573a0223b32240ac7640b5cd3342c45f89dc4
vrunda310 <vrundam@zenithive.com>
2026-01-08 13:08:59 +0530

    Initial commit

---
+++ ui-lm_portal_backend/src/api/quiz-submission/routes/quiz-submission.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * quiz-submission router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::quiz-submission.quiz-submission');
